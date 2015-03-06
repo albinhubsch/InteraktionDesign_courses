84e9a05e9a936edf30232b620737b727a2c2fad5
Nicole Martini <nicole.martini.1992@gmail.com>
2015-03-06 21:29:07 +0100

    minor changes

--- resources/views/course.blade.php
+++ resources/views/course.blade.php
@@ -2,9 +2,9 @@
 
 @section('jumbotron')
 	
-	<h2 class ="single-course-title"> 
-	{{{ $course->name }}}
-	</h2>
+	<div class ="single-course-title"> 
+	<h2>{{{ $course->name }}}</h2>
+	</div>
 		<div class="course-list-item2">
 
 			<span>
@@ -29,8 +29,14 @@
 @section('content')
 <div class="row">
 	<div class="col-md-9">
-		{{{ $course->description }}}
+		<h3>Kursbeskrivning</h3>
+			
+			<span class="course-single-page-info">
+			{{{ $course->description }}}
+
+			</span>
 	</div>
+
 	<div class="col-md-3">
 		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic corporis inventore rem perferendis reiciendis aspernatur obcaecati, facilis labore quam soluta blanditiis voluptas, modi rerum aperiam libero odio magni, porro odit.
 	</div>

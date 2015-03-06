e4bb13e9210a43b9ee50410e296bf41f48b50735
Victor Winnhed <Victor.winnhed@gmail.com>
2015-03-06 21:43:57 +0100

    Buttons added not styled tho

--- resources/views/courses.blade.php
+++ resources/views/courses.blade.php
@@ -5,6 +5,64 @@
 	<input id="jumbotron_search_field" name="jumbotron_search_field" class=".col-xs-12 .col-md-8" type="text">
 	<br><br>
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi quo ad voluptas, cupiditate eos autem.</p>
+	<div class="btn-group">
+		<button type="button" class="btn btn-default dropdown-toggle" aria-expanded="false" >
+		 HT 
+ 		 </button>
+ 	</div>
+ 	<div class="btn-group">
+ 		 <button type="button" class="btn btn-default dropdown-toggle" aria-expanded="false" >
+		 VT 
+ 		 </button>
+ 	</div>	 
+	<div class="btn-group">
+ 		 <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+    		Studietakt <span class="caret"></span>
+  		</button>
+  		<ul class="dropdown-menu" role="menu">
+    		<li><a href="#">25%</a></li>
+    		<li><a href="#">50%</a></li>
+    		<li><a href="#">100%</a></li>
+    		<li class="divider"></li>
+    		<li><a href="#">Här kan man också ha nått</a></li>
+  		</ul>
+  	</div>
+	<div class="btn-group">
+  		 <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+    		Läsperiod <span class="caret"></span>
+  		</button>
+  		<ul class="dropdown-menu" role="menu">
+    		<li><a href="#">HT period 1</a></li>
+    		<li><a href="#">HT period 2</a></li>
+    		<li><a href="#">VT Period 3</a></li>
+    		<li><a href="#">VT Period 4</a></li>
+    		<li class="divider"></li>
+    		<li><a href="#">Här kan man också ha nått</a></li>
+  		</ul>
+	</div>
+	<div class="btn-group">
+  		 <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+    		Högskolepoäng <span class="caret"></span>
+  		</button>
+  		<ul class="dropdown-menu" role="menu">
+    		<li><a href="#">7.5 hp</a></li>
+    		<li><a href="#">9 hp</a></li>
+    		<li><a href="#">15 hp</a></li>
+    		<li class="divider"></li>
+    		<li><a href="#">Här kan man också ha nått</a></li>
+  		</ul>
+	</div>
+	<div class="btn-group">
+  		 <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+    		Nivå <span class="caret"></span>
+  		</button>
+  		<ul class="dropdown-menu" role="menu">
+    		<li><a href="#">Grundnivå ej nybörjare</a></li>
+    		<li><a href="#">Avancerad nivå</a></li>
+    		<li class="divider"></li>
+    		<li><a href="#">Här kan man också ha nått</a></li>
+  		</ul>
+	</div>
 @endsection
 
 @section('content')
